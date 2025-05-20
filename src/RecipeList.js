e234e27df7b5c8900506ad246f44f9b4d442e2ba
nakyun <nakyun22@naver.com>
2025-05-20 23:35:18 +0900

    상품 목록 이름 표시 수정 (title → name)

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-function RecipeList({ recipes, onSelect }) {
+function RecipeList({ recipes }) {
   return (
     <div>
       <h2>상품 목록</h2>
       {recipes.map((recipe, index) => (
         <div key={index}>
-          <button>{recipe.name}</button>
+          <button>{recipe.name}</button> {/* ← 'name' 사용 */}
         </div>
       ))}
     </div>
   );
 }
+
 
 export default RecipeList;
